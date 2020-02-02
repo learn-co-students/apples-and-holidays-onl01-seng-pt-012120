c2141e7bd63449865042e39826caf5713e5b7337
James Batsell <jamesbatsell05@gmail.com>
2020-02-02 21:22:08 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,7 +17,9 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
-  # return the second element in the 4th of July array
+  holiday_hash.each do |season, holiday|
+    
+  end
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
